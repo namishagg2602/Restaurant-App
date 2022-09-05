6c3bad12f3a3b429c88ac606b07ee47578aa9183
Namish Aggarwal <56684605+namishagg2602@users.noreply.github.com>
2022-09-05 19:19:01 +0530

    pushed to master

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Homepage from './Homepage';
+
+function App() {
+  return (
+    <Homepage/>
+  );
+}
+
+export default App;
---
+++ src/components/Homepage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const Homepage = () =>
+{
+
+    return (
+    <div>
+        <h1>Welcome to our Restaurant</h1>
+
+        <div>
+        <button>Customer</button>
+        </div>
+
+        <div>
+        <button>Admin</button>
+        </div>
+
+    </div>
+    );
+    
+};
+
+
+export default Homepage; 
