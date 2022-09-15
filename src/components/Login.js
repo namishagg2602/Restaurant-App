ff8f65e036d7aac527ed751a2950b9bd99856578
Priya19999 <46641222+Priya19999@users.noreply.github.com>
2022-09-15 16:14:56 +0530

    Add files via upload

---
+++ src/components/Login.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { useRef } from 'react';
+import AdminNavbar from './AdminNavbar';
+import AdminHome from './AdminHome';
+import Button from 'react-bootstrap/Button';
+import { signup, login, logout, useAuth } from '../firebase';
+const Login = () => {
+    const[loading, setLoading] = useState(false);
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const currentUser = useAuth();
+
+    async function handleSignUp()
+    {
+        setLoading(true);
+        try{
+            await signup(emailRef.current.value,passwordRef.current.value);
+        } catch{
+            alert("Error");
+        }
+    }
+    async function handleLogIn()
+    {
+        setLoading(true);
+        try{
+            await login(emailRef.current.value,passwordRef.current.value);
+        } catch{
+            alert("Incorrect Email/Password");
+        }
+    }
+    async function handleLogout() {
+        setLoading(true);
+
+        try {
+            await logout();
+        } catch{
+            alert("Error!");
+        }
+        setLoading(false);
+    }
+    return(
+        <div>
+            <div>Currently Logged in as: {currentUser?.email}</div>
+            <div>
+<input ref={emailRef}placeholder='Email'/>
+<br></br>
+<input ref={passwordRef} type="password" placeholder='Password'/>
+
+</div>
+
+<button disabled = {currentUser } onClick={handleLogIn}> Log In</button>
+<button disabled ={!currentUser}onClick={handleLogout}> LogOut</button>
+</div>
+
+    )
+}
+export default Login;
