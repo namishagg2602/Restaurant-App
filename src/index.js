c8748628babc43cde7493206003db8818f932092
MeghaKanojia <68979597+MeghaKanojia@users.noreply.github.com>
2022-09-19 16:30:40 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import img from './thisIsFinal.png'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div style={{ backgroundImage: `url(${img})`, backgroundRepeat: 'no-repeat', backgroundSize:'cover'}}>
       <App />
+    </div>
   </React.StrictMode>
 );
 
